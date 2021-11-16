496b82fd729acae91b27da0d1649c6a25c55f1ab
Eric Tam <eric.m2hk@gmail.com>
2021-11-16 05:14:00 -0800

    add navbar

---
+++ client-app/src/app/layout/NavBar.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Container, Menu, Button } from "semantic-ui-react";
+
+function NavBar() {
+  return (
+    <Menu inverted fixed="top">
+      <Container>
+        <Menu.Item header>
+          <img
+            src="/assets/logo.png"
+            alt="logo"
+            style={{ marginRight: "10px" }}
+          />
+          Reactivities
+        </Menu.Item>
+        <Menu.Item name="Activities"></Menu.Item>
+        <Menu.Item>
+          <Button positive content="Create Activity"></Button>
+        </Menu.Item>
+      </Container>
+    </Menu>
+  );
+}
+
+export default NavBar;
