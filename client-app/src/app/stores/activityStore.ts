be303a37d6e55f9cfdfa034dd98148b7a1972a60
Eric Tam <eric.m2hk@gmail.com>
2021-11-21 02:46:43 -0800

    using mobx to load activities and loadinginitial

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,13 +1,35 @@
-import { action, makeAutoObservable, makeObservable, observable } from "mobx";
+import { makeAutoObservable } from "mobx";
+import agent from "../api/agent";
+import { Activity } from "../models/activity";
 
 export default class ActivityStore {
-  title = "Hello from MobX!";
+  activities: Activity[] = [];
+  selectedActivity: Activity | null = null;
+  editMode: boolean = false;
+  loading: boolean = false;
+  loadingInitial: boolean = false;
+  submitting: boolean = false;
 
   constructor() {
     makeAutoObservable(this);
   }
 
-  setTitle = () => {
-    this.title = this.title + "!";
+  loadActivities = async () => {
+    this.setLoadingInitial(true);
+    try {
+      const activities = await agent.Activities.list();
+      activities.forEach((activity) => {
+        activity.date = activity.date.split("T")[0];
+        this.activities.push(activity);
+      });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      this.setLoadingInitial(false);
+    }
+  };
+
+  setLoadingInitial = (state: boolean) => {
+    this.loadingInitial = state;
   };
 }
